8025b08608536b5cc48d5af01774135e9efef586
AJP-ARCHIVE <angelajpark93@gmail.com>
2026-08-30 13:32:15 -0400

    Update sendServerResponse.js
    Removed commented out codes that were used for testing.

--- utils/sendServerResponse.js
+++ utils/sendServerResponse.js
@@ -4,9 +4,5 @@ import fs from 'node:fs'
 export function sendServerResponse(res, status, contentType, content) {
     res.statusCode = status 
     res.setHeader('Content-Type', contentType)
-    // if (contentType === 'text/plain') {
-    //     // have log file downloaded as an attachment 
-    //     res.setHeader('Content-Disposition', 'attachment; filename="Gold-Transaction.txt')
-    // }
     res.end(content)
 }
