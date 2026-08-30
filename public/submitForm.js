b605e4e6aa9d0b2d8eb4a469348731babf9921e1
AJP-ARCHIVE <angelajpark93@gmail.com>
2026-08-30 14:08:09 -0400

    Update submitForm.js
    Removed commented out codes that were used not used. Commented out console.log codes. Updated error handling for server response.

--- public/submitForm.js
+++ public/submitForm.js
@@ -27,35 +27,19 @@ form.addEventListener("submit", async (e) => {
             body: JSON.stringify({date, price, amount, goldAmount})
         }) 
         if (response.ok) {
-            console.log('Form submitted')
-            console.log({date,price,amount})
+            // console.log('Form submitted')
+            // console.log({date,price,amount})
             
             
             // get http response body -> convert to Blob (Binary Large Object) 'get raw data'
             blob = await response.blob();
             const goldSold = calculateGoldAmount(price, amount)
             dialogModal.showModal()
-            modalSummary.textContent = `You just bought ${goldSold} (ozt) for $${amount}. \n You will receive documentation shortly.`
-            //handleModal(blob)
+            modalSummary.textContent = `You just bought ${goldSold} (ozt) for $${amount}. \n You will receive documentation shortly.`            
 
-            
-
-            // // create browser url to Blob 'file generated in memory'
-            // const url = URL.createObjectURL(blob);
-            
-            // const link = document.createElement("a");
-            // link.href = url;
-            // link.download = "Gold-Transaction.txt";
-
-            // document.body.appendChild(link);
-            // // trigger and clear the link 
-            // link.click();
-            // link.remove();
-            // // Clear Blob
-            // URL.revokeObjectURL(url);
         }
         else {
-            console.log('Server error')
+            throw new Error('Server error')
         }
     }
     catch (err) {
@@ -66,82 +50,7 @@ form.addEventListener("submit", async (e) => {
     }
 })
 
-// form.addEventListener('submit', async function(event) {
-//     event.preventDefault()
 
-//     const amount = document.getElementById('investment-amount').value 
-//     const price = document.getElementById('price-display').value
-//     const date = new Date().toISOString()
-
-//     const formData = {
-//         date,
-//         price,
-//         amount
-//     }
-
-//     try {
-//         const response = await fetch('/api', {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({date, price, amount})
-//         }) 
-//         if (response.ok) {
-//             console.log('Form submitted')
-//         }
-//         else {
-//             console.log('Server error')
-//         }
-//     }
-//     catch (err) {
-//         console.error(err)
-//     }
-
-    
-// })
-
-// const form = document.getElementById("gold-form");
-
-// form.addEventListener("submit", async (event) => {
-//     event.preventDefault();
-
-//     console.log("SUBMIT HANDLER FIRED");
-
-//     const amount = document.getElementById("investment-amount").value;
-//     const price = document.getElementById("price-display").value;
-//     const date = new Date().toISOString();
-
-//     console.log({
-//         date,
-//         price,
-//         amount
-//     });
-
-//     try {
-//         const response = await fetch("/api", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 date,
-//                 price,
-
-//             })
-//         });
-
-//         console.log("Response:", response);
-
-//         if (response.ok) {
-//             console.log("Form submitted");
-//         } else {
-//             console.log("Server error");
-//         }
-//     } catch (err) {
-//         console.error(err);
-//     }
-// });
 
 function calculateGoldAmount(price, amount) {
 // ounces (oz) of gold = total money invested 'purchased' / gold price per oz
